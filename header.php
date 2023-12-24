1beaf670e8993a694efaa1b212c1b000dbda7a52
Ram <creativerampm@gmail.com>
2023-12-24 23:56:12 +0530

    menu link change

--- header.php
+++ header.php
@@ -32,10 +32,10 @@
 
       <nav id="nav-menu-container">
         <ul class="nav-menu">
-          <li class="menu-active"><a href="#body">Home</a></li>
-          <li><a href="#about">About Us</a></li>
-          <li><a href="#services">Services</a></li>
-          <li><a href="#porject">Portfolio</a></li>
+          <li class="menu-active"><a href="https://creativewebstech.com/">Home</a></li>
+          <li><a href="https://creativewebstech.com/#about">About Us</a></li>
+          <li><a href="https://creativewebstech.com/#services">Services</a></li>
+          <li><a href="https://creativewebstech.com/#porject">Portfolio</a></li>
           <li class="menu-has-children" style="display: none;"><a href="">Drop Down</a>
             <ul>
               <li><a href="#">Drop Down 1</a></li>
@@ -44,8 +44,8 @@
               <li><a href="#">Drop Down 5</a></li>
             </ul>
           </li>
-          <li><a href="/blogs/">Blogs</a></li>
-          <li><a href="#contact">Contact</a></li>
+          <li><a href="https://creativewebstech.com/blogs/">Blogs</a></li>
+          <li><a href="https://creativewebstech.com/#contact">Contact</a></li>
         </ul>
       </nav><!-- #nav-menu-container -->
     </div>
