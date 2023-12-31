d4fa38552bd542b5095120c967e52a78508c7d24
Ram <creativerampm@gmail.com>
2024-01-01 01:18:24 +0530

    Made changs to add blog and styling of clients logo section

--- home.php
+++ home.php
@@ -5,21 +5,21 @@
 <!-- Get Basic Content Template -->
 <?php //get_template_part('template-parts/most-popular-news'); ?>
 <div id="content" role="main">
-		<?php while ( have_posts() ) : the_post(); ?>     
-			<?php // Get Standard Comp	onents
-				if( have_rows('basic-content') ):
+	<?php while ( have_posts() ) : the_post(); ?>     
+		<?php // Get Standard Comp	onents
+			if( have_rows('basic-content') ):
 
-					while ( have_rows('basic-content') ) : the_row();
+				while ( have_rows('basic-content') ) : the_row();
 
-						$section_path = 'template-parts/'.get_row_layout();
+					$section_path = 'template-parts/'.get_row_layout();
 
-						get_template_part($section_path);
+					get_template_part($section_path);
 
-					endwhile;
+				endwhile;
 
-				endif; ?>	
+			endif;
+		?>	
 	<?php endwhile; // end of the loop. ?>
-
+	<?php //get_template_part('template-parts/full-width-blogs'); ?>
 </div><!-- #content -->
 <?php get_footer(); ?>
-  
